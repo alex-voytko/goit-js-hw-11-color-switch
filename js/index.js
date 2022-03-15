0103a957ecc92c985c1c9a9ed09b9dd58284ef74
alex-voytko <voytko1994@gmail.com>
2022-03-15 22:14:22 +0200

    1.0.0

---
+++ js/index.js
@@ -0,0 +1,34 @@
+let timeId = null;
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+const randomIntegerFromInterval = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const refs = {
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+};
+
+refs.startBtn.addEventListener("click", startColorize);
+refs.stopBtn.addEventListener("click", stopColorize);
+
+function startColorize() {
+  timeId = setInterval(colorizeBody, 1000);
+  refs.startBtn.setAttribute("disabled", true);
+}
+
+function colorizeBody() {
+  const randomIndex = randomIntegerFromInterval(0, colors.length - 1);
+  document.body.style.backgroundColor = colors[randomIndex];
+}
+
+function stopColorize() {
+  clearInterval(timeId);
+  refs.startBtn.removeAttribute("disabled");
+}
